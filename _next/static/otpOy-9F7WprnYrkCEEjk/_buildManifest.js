c438c577743ce591b987e10c105d22f142399e1a
Josef Krein <fvjosef21@outlook.com>
2024-03-23 22:18:14 +0800

    Updates

---
+++ _next/static/otpOy-9F7WprnYrkCEEjk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-539e85e06691ad33.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
